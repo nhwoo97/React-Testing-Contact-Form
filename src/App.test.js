8c5fb30ee04d947d8fdc1e20a8e66f51100d0dd0
Namhyuk Woo <namhyukwoo@Namhyuks-Air.lan>
2020-09-04 00:34:40 -0700

    Finished

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm'
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+const breakMyApp = ()=> {
+  throw new Error('Error has been created')
+}
+
+test('input field works', ()=> {
+  render(<ContactForm/>)
+
+  const firstName = screen.getByLabelText(/First Name*/i)
+  const lastName = screen.getByLabelText(/Last Name*/i)
+  const email = screen.getByLabelText(/email*/i)
+  const message = screen.getByLabelText(/message*/i)
+  
+  expect(firstName).toBeTruthy()
+  expect(lastName).toBeTruthy()
+  expect(email).toBeTruthy()
+  expect(message).toBeTruthy()
+
+  fireEvent.change(firstName, {target: { value:'Nam'}})
+  fireEvent.change(lastName, {target: { value:'Woo'}})
+  fireEvent.change(email, {target: { value:'email@email.com'}})
+  fireEvent.change(message, {target: { value:'ramdom things here'}})
+
+  const form = screen.getByText(/FORM/i)
+  expect(form).toBeTruthy()
+  fireEvent.click(form)
+
+})
